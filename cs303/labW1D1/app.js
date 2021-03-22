56ac3a77cfe1cefcd30d0d1a182711caf55d9f07
Sosi <fekade.1979@gmail.com>
2021-03-22 19:58:07 -0400

    cs303

--- cs303/labW1D1/app.js
+++ cs303/labW1D1/app.js
@@ -3,21 +3,21 @@
 "use strict";
 
 function pow(x, n) {
-	return x * n;
+  return x ** n;
 }
 
 function add2(n1, n2) {
-	return n1 + n2;
+  return n1 + n2;
 }
 
 function add(...args) {
-	let count = arguments.length;
-	let sum = 0;
-	for (let i=0; i<count; i++) {
-		sum += arguments[i];		
-	}
-	
-	return sum;
+  let count = arguments.length;
+  let sum = 0;
+  for (let i = 0; i < count; i++) {
+    sum += arguments[i];
+  }
+
+  return sum;
 }
 
 /*
@@ -30,6 +30,18 @@ function add(...args) {
 	0 - 59   NC
 */
 function computeGrade(grades) {
-	return "???";
+  for (let grade of grades) {
+    if (grade >= 90 && grade <= 100) {
+      return "A";
+    } else if (grade >= 80) {
+      return "B";
+    } else if (grade >= 70) {
+      return "C";
+    } else if (grade >= 60) {
+      return "D";
+    } else if (grade < 60 && grade >= 0) {
+      return "NC";
+    }
+  }
 }
-
+module.exports = { pow, add2, add, computeGrade };
