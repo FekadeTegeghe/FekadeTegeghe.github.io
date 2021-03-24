3a09f82ba75d316cf10a1ec147aab240c0cbb999
Sosi <fekade.1979@gmail.com>
2021-03-24 11:03:59 -0400

    updated

--- cs303/labW1D2/app.js
+++ cs303/labW1D2/app.js
@@ -1,48 +1,25 @@
 /* eslint-disable require-jsdoc */
 /* eslint-disable no-unused-vars */
 "use strict";
-
 // See tests.js for behavior
-// function nameString(obj) {
-//   let prop = [];
-//   let reversed;
-//   let j = 0;
-//   if (obj === {}) {
-//     return "Object has no property";
-//   } else if (typeOf(obj) === undefined) {
-//     return "No object passed";
-//   } else if (obj === null) {
-//     return "Null argument";
-//   } else if (obj !== null || typeOf(obj) !== undefined) {
-//     for (let object in obj) {
-//       if (Object.keys(obj).length === 1) {
-//         return Object.keys(obj);
-//       } else {
-//         prop.push(object);
-//
-//       }
-//			prop.sort();
-//         prop.join(".");
-//     }
-//     return prop;
-//   }
-// }
-// module.exports = { nameString };
-
+/**
+ *
+ * @param {object} obj is an object
+ * @returns {string} is a string
+ */
 function nameString(obj) {
   let prop = [];
-  if (obj === {}) {
+  if (obj === undefined) return "No object passed";
+  if (obj === null) return "Null argument";
+  for (let key in obj) {
+    prop.push(key);
+  }
+  if (prop.length === 0) {
     return "object has no properties";
-  } else if (obj === undefined) {
-    return "No object passed";
-  } else if (obj === null) {
-    return "null argument";
   } else {
-    for (let key in obj) {
-      prop.push(key);
-    }
     prop.sort();
     return prop.join(".");
   }
 }
+
 module.exports = { nameString };
