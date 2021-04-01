9d54f8e8ea8a4e49ab501a6a52b1194e9bc6d54a
Sosi <fekade.1979@gmail.com>
2021-04-01 18:54:59 -0400

    Update linkedList.js

--- cs303/w2d3HW/linkedList.js
+++ cs303/w2d3HW/linkedList.js
@@ -23,22 +23,23 @@ function add(num) {
 }
 console.log(add(10));
 //Question2
-
+//creating an empty linkedList
 let linkedList = {};
+//adding elements to the linked list
 linkedList.next = { value: 1 };
 linkedList.next.next = { value: 2 };
 linkedList.next.next.next = { value: 5 };
 linkedList.next.next.next.next = { value: 2 };
 linkedList.next.next.next.next.next = { value: 7, next: null };
-
+//Printing the linked list befor deletion
 console.log(`linkedList {${printList(linkedList.next)}}`);
-
+//Deleting the first node
 let node = linkedList.next.next;
 linkedList.next = node;
 node = null;
-
+//printing the list after deletion of an element
 console.log(`linkedList {${printList(linkedList.next)}}`);
-
+//Working on recursive function.
 function printList(linkedList) {
   if (linkedList.next == null) {
     return linkedList.value;
